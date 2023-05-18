4150d9166cc9f3cfd7f3cddd4f566d99f06a1597
rinkuahamed <ringkuali18@gmail.com>
2023-05-18 23:34:30 +0600

    Gallery section design with responsive and some changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "./Banner/Banner";
+import Galleries from "./Gallery/Galleries";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Galleries></Galleries>
     </div>
   );
 };
