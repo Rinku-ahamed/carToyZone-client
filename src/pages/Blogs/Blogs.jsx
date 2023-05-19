4c0460fea5f87a05c5c2da683142253bc5d4fe53
rinkuahamed <ringkuali18@gmail.com>
2023-05-19 23:39:48 +0600

    Create blog page and some content and make responsive

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -6,7 +6,7 @@ const Blogs = () => {
         <div className="hero-overlay bg-opacity-80"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="w-[500px]">
-            <h2 className="text-3xl font-semibold mb-5">Blog Pages</h2>
+            <h2 className="text-3xl font-semibold mb-5">Blog Page</h2>
           </div>
         </div>
       </div>
@@ -36,23 +36,58 @@ const Blogs = () => {
             Compare SQL and NoSQL databases?
           </h2>
           <p className="mt-4 text-xl text-[#757575]">
-            In this tutorial we going to discuss about
+            In this tutorial we going to discuss about The decision of which
+            type of database to use - SQL or NoSQL - will depend on the
+            particular needs and requirements of the project. For example, SQL
+            databases are vertically scalable, while NoSQL databases are
+            horizontally scalable. SQL databases are table-based, while NoSQL
+            databases are document, key-value, graph, or wide-column stores.SQL
+            databases are better for multi-row transactions, while NoSQL is
+            better for unstructured data like documents or JSON.
           </p>
         </article>
         <article className="shadow-lg py-10 px-6 mt-16">
           <h2 className="text-3xl font-semibold">
-            What is express js? What is Nest JS (google it)?
+            What is express js? What is Nest JS?
           </h2>
           <p className="mt-4 text-xl text-[#757575]">
-            In this tutorial we going to discuss about
+            In this tutorial we going to discuss about express vs nest js, When
+            we want to build a large-scale application with a framework,
+            Express.js is usually the first thing that comes to mind. Express
+            has gained popularity in the last few years among developers. NestJS
+            is a progressive Node.js framework for building efficient, scalable,
+            and reliable server-side applications. It is built on top of
+            Express.js and provides a more structured way of building
+            applications with TypeScript. NestJS uses a modular architecture,
+            making it easy to maintain and scale applications as they grow.
+            TypeScript support: NestJS is built on top of TypeScript, which
+            provides strong typing and better code organization. This makes it
+            easier to maintain and scale large applications on the other hand
+            Lightweight: Express is a lightweight framework that is easy to set
+            up and use. It does not have many dependencies, making it ideal for
+            small projects.
           </p>
         </article>
         <article className="shadow-lg py-10 px-6 mt-16">
           <h2 className="text-3xl font-semibold">
-            What is MongoDB aggregate and how does it work (google it)?
+            What is MongoDB aggregate and how does it work?
           </h2>
           <p className="mt-4 text-xl text-[#757575]">
-            In this tutorial we going to discuss about
+            In this tutorial we going to discuss about MongoDB aggregate,
+            Aggregation is a way of processing a large number of documents in a
+            collection by means of passing them through different stages. The
+            stages make up what is known as a pipeline. As with many other
+            database systems, MongoDB allows you to perform a variety of
+            aggregation operations. These allow you to process data records in a
+            variety of ways, such as grouping data, sorting data into a specific
+            order, or restructuring returned documents, as well as filtering
+            data as one might with a query. In MongoDB, the aggregation pipeline
+            consists of stages and each stage transforms the document. Or in
+            other words, the aggregation pipeline is a multi-stage pipeline, so
+            in each state, the documents taken as input and produce the
+            resultant set of documents now in the next stage(id available) the
+            resultant documents taken as input and produce output, this process
+            is going on till the last stage
           </p>
         </article>
       </div>
